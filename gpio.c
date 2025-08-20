d0a02e5fd7bec13cb04e07e066d57599f51904aa
John Bryan Valle <jbvalle.jv@gmail.com>
2025-08-21 01:38:55 +0200

    Implemented Gpio Init, read, write

---
+++ gpio.c
@@ -0,0 +1,24 @@
+#include "gpio.h"
+#include "stm32f401re_regs.h"
+
+#define MODER_WIDTH 2
+
+void gpio_init(const GpioPinConfigType * config, uint8_t count){
+
+    for(uint8_t i = 0; i < count; i++){
+
+        RCC->AHB1ENR |= (config[i].port + 1);
+        GpioPorts[config[i].port]->MODER &= ~(3 << (config[i].pin * MODER_WIDTH));
+        GpioPorts[config[i].port]->MODER |=  (config[i].mode << (config[i].pin * MODER_WIDTH));
+    }
+}
+
+void gpio_write(uint8_t port, uint8_t pin, GpioLevelType level){
+
+    GpioPorts[port]->ODR &= ~(1 << pin);
+    GpioPorts[port]->ODR |= ~(level << pin);
+}
+GpioLevelType gpio_read(uint8_t port, uint8_t pin){
+
+    return GpioPorts[port]->IDR;
+}
---
+++ gpio.h
@@ -0,0 +1,10 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include "cfg_gpio.h"
+
+void gpio_init(const GpioPinConfigType * config, uint8_t count);
+void gpio_write(uint8_t port, uint8_t pin, GpioLevelType level);
+GpioLevelType gpio_read(uint8_t port, uint8_t pin);
+
+#endif
