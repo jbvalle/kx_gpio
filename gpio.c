e351d48c10cc3813a6d1475e275b8d474f813382
John Bryan Valle <jbvalle.jv@gmail.com>
2025-08-23 21:10:21 +0200

    added toggle functionality

--- gpio.c
+++ gpio.c
@@ -7,7 +7,7 @@ void gpio_init(const GpioPinConfigType * config, uint8_t count){
 
     for(uint8_t i = 0; i < count; i++){
 
-        RCC->AHB1ENR |= (config[i].port + 1);
+        RCC->AHB1ENR |= (1 << config[i].port);
         GpioPorts[config[i].port]->MODER &= ~(3 << (config[i].pin * MODER_WIDTH));
         GpioPorts[config[i].port]->MODER |=  (config[i].mode << (config[i].pin * MODER_WIDTH));
     }
@@ -18,6 +18,12 @@ void gpio_write(uint8_t port, uint8_t pin, GpioLevelType level){
     GpioPorts[port]->ODR &= ~(1 << pin);
     GpioPorts[port]->ODR |= ~(level << pin);
 }
+
+void gpio_toggle(uint8_t port, uint8_t pin){
+
+    GpioPorts[port]->ODR ^= (1 << pin);
+}
+
 GpioLevelType gpio_read(uint8_t port, uint8_t pin){
 
     return GpioPorts[port]->IDR;
--- gpio.h
+++ gpio.h
@@ -5,6 +5,7 @@
 
 void gpio_init(const GpioPinConfigType * config, uint8_t count);
 void gpio_write(uint8_t port, uint8_t pin, GpioLevelType level);
+void gpio_toggle(uint8_t port, uint8_t pin);
 GpioLevelType gpio_read(uint8_t port, uint8_t pin);
 
 #endif
